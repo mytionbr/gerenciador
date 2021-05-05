4c90b1e2a2c4e327b3c1dcb3496819a89a3cb9d4
mytionBr <matheuss.jesus2017@gmail.com>
2021-05-05 17:05:32 -0300

    initial commit

---
+++ src/br/com/alura/gerenciador/filter/HomeFilter.java
@@ -0,0 +1,43 @@
+package br.com.alura.gerenciador.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class HomeFilter
+ */
+@WebFilter("/HomeFilter")
+public class HomeFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse rep = (HttpServletResponse) response;
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/home.jsp");
+		rd.forward(req, rep);
+	
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
